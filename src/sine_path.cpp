ccf7606ba62437544bbb2eb2d7eece7e35aae447
beyza <beyzanurkya98@gmail.com>
2021-08-13 13:59:40 +0300

    add timer_callback function in sine_path

--- src/sine_path.cpp
+++ src/sine_path.cpp
@@ -14,47 +14,41 @@ public:
     SinePath() : Node("sinePath"){
 
         poses_publisher = this->create_publisher<geometry_msgs::msg::PoseArray>("sine_path", 10);
-
-
-        auto publish_msg = [this]() -> void {
-            auto message = geometry_msgs::msg::PoseArray();
-            geometry_msgs::msg::PoseArray poseArray;
-            geometry_msgs::msg::Pose p;
-
-            poseArray.header.frame_id = "map";
-
-            for(int i=0;i<=360;i++){
-                p.position.x  = static_cast<float>(i);
-                p.position.y = 10*sin(static_cast<float>(i) * M_PI / 5);
-                p.position.z = 0;
-                p.orientation.x  = 1;
-                p.orientation.y =  1;
-                p.orientation.z = 0;
-                p.orientation.w = 1;
-                poseArray.poses.push_back(p);
-                message = poseArray;
-                poses_publisher->publish(message);
-
-
-
-
-
-            }
-        };
-        timer_ = this->create_wall_timer(1s, publish_msg);
+        timer_ = this->create_wall_timer(500ms, std::bind(&SinePath::timer_callback, this));
     }
 
 private:
+    void timer_callback()
+    {
+
+        auto message = geometry_msgs::msg::PoseArray();
+        geometry_msgs::msg::PoseArray poseArray;
+        geometry_msgs::msg::Pose p;
+        poseArray.header.frame_id = "map";
+        poseArray.header.stamp = this->now();
+
+        for(int i=0;i<=360;i++){
+            p.position.x  = static_cast<float>(i);
+            p.position.y = 10*sin(static_cast<float>(i) * M_PI / 5);
+            p.position.z = 0;
+            p.orientation.x  = 1;
+            p.orientation.y =  1;
+            p.orientation.z = 0;
+            p.orientation.w = 1;
+            poseArray.poses.push_back(p);
+        }
+        message = poseArray;
+        poses_publisher->publish(message);
+    }
 
     rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr poses_publisher;
     rclcpp::TimerBase::SharedPtr timer_;
 };
 
-
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(make_shared<SinePath>());
+    rclcpp::spin(std::make_shared<SinePath>());
     rclcpp::shutdown();
     return 0;
 }
