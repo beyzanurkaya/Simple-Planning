b603331c19bf8ed4f84e5f581837b36ffd41cb61
beyza <beyzanurkya98@gmail.com>
2021-08-16 16:00:32 +0300

    add rviz2 node

--- launch/planning.launch.py
+++ launch/planning.launch.py
@@ -4,6 +4,11 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2"
+        ),
+        Node(
             package='motion_planning',
             executable='sine_path',
             output='screen'
